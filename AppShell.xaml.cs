fd85b5a97f25871abd5bba2d8abd047d85a77cfe
Reece Mackie <reece.mackie@sword-group.com>
2025-07-10 16:35:13 +0100

    Create and prune template app

---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MAUIAndroidTabBarIssue
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
